0698127bc8ab420a6e969e2080087b98e0dd97c9
robo <30987265+Robonau@users.noreply.github.com>
2026-06-03 19:07:08 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,14 +14,14 @@ export default defineConfig({
     },
     server: {
         proxy: {
-            // String shorthand: any request starting with /api/graphql
-            // will be sent to http://localhost:456/api/graphql
-            "/api/graphql": {
+            // String shorthand: any request starting with /api
+            // will be sent to http://localhost:456/api
+            "/api": {
                 // static-port
                 target: "http://localhost:4567",
                 changeOrigin: true,
-                // Optional: If your backend doesn't have /api/graphql in its actual path,
-                // you can remove it using rewrite: (path) => path.replace(/^\/api\/graphql/, '')
+                // Optional: If your backend doesn't have /api in its actual path,
+                // you can remove it using rewrite: (path) => path.replace(/^\/api/, '')
             },
         },
     },
